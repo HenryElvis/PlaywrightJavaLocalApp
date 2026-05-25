42bce4ba675a7f907ce061219f2592dd442e7312
HenryElvis <elvis.henry@sogeti.com>
2026-05-25 15:34:29 +0200

    init projet and add some stuff

---
+++ playwright-test/src/test/java/com/capgemini/AppLocalTest.java
@@ -0,0 +1,111 @@
+package com.capgemini;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import com.microsoft.playwright.options.AriaRole;
+
+public class AppLocalTest 
+{
+    private static Playwright playwright;
+    private static Browser browser;
+    private Page page;
+
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+    }
+
+    @BeforeEach
+    void createContextAndPage() {
+        this.page = browser.newPage();
+        this.page.navigate("http://localhost:4200/pages/iot-dashboard");
+    }
+
+    @AfterEach
+    void closeContext() {
+        this.page.close();
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        if (browser != null) {
+            browser.close();
+        }
+
+        if (playwright != null) {
+            playwright.close();
+        }
+    }
+
+    // ### UTILS ###
+
+    void clickOnText(String _text)
+    {
+        page.getByText(_text).click();
+    }
+
+    // ### TESTS ###
+
+    @Test
+    void testTitle() {
+        String title = page.title();
+        assertThat(page).hasTitle(title);
+    }
+
+    @Test
+    void headerVisible() {
+        assertTrue(true);
+    }
+
+    @Test
+    void clickOnForms()
+    {
+        clickOnText("Forms");
+    }
+
+    @Test
+    void clickOnFormLayout()
+    {
+        clickOnText("Forms");
+        clickOnText("Form Layout");
+    }
+
+    @Test
+    void testChildElement()
+    {
+        clickOnText("Forms");
+        clickOnText("Form Layout");
+        page.locator("nb-card nb-radio :text-is('Option 2')").click();
+
+        assertTrue(true);
+
+        page.locator("nb-card").nth(3).getByRole(AriaRole.BUTTON).click();
+
+        assertTrue(true);
+    }
+
+    @Test
+    void testParentElement()
+    {
+        clickOnText("Forms");
+        clickOnText("Form Layout");
+
+        Locator siblingTitle = page.locator("nb-card:has-text('Using the Grid')");
+
+        siblingTitle.getByPlaceholder("Email").click();
+
+        assertTrue(true);
+    }
+}
