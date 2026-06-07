4e50b185e78b5690d843a16a690f0e0ec5372653
HenryElvis <elvis.henry@sogeti.com>
2026-06-07 11:30:59 +0200

    param methods

--- playwright-test/src/test/java/com/capgemini/pages/FormLayoutPage.java
+++ playwright-test/src/test/java/com/capgemini/pages/FormLayoutPage.java
@@ -1,9 +1,8 @@
 package com.capgemini.pages;
 
-import com.capgemini.tools.BaseTools;
 import com.microsoft.playwright.Locator;
-import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Locator.CheckOptions;
+import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
 
 public class FormLayoutPage {
@@ -11,12 +10,15 @@ public class FormLayoutPage {
     private final Page page;
 
     Locator containerGridCard;
+    Locator containerInlineFormCard;
 
     public FormLayoutPage(Page _page) 
     {
         this.page = _page;
 
         containerGridCard = page.locator("nb-card").filter(new Locator.FilterOptions().setHasText("Using the Grid"));
+
+        containerInlineFormCard = page.locator("nb-card").filter(new Locator.FilterOptions().setHasText("Inline form"));
     }
 
     public void fillingFormWithEmailAndPassword(String _email, String _password, String option)
@@ -26,5 +28,18 @@ public class FormLayoutPage {
         containerGridCard.getByRole(AriaRole.TEXTBOX, new Locator.GetByRoleOptions().setName("Password")).fill(_password);
 
         containerGridCard.getByRole(AriaRole.RADIO, new Locator.GetByRoleOptions().setName(option)).check(new CheckOptions().setForce(true));
+
+        containerGridCard.getByRole(AriaRole.BUTTON, new Locator.GetByRoleOptions().setName("Sign in")).click();
+    }
+
+    public void fillingInlineFormWithEmailAndPassword(String _name, String _email, Boolean rememberUser)
+    {
+        containerInlineFormCard.getByPlaceholder("Jane Doe").fill(_name);
+
+        containerInlineFormCard.getByRole(AriaRole.TEXTBOX, new Locator.GetByRoleOptions().setName("Email")).fill(_email);
+
+        containerInlineFormCard.getByRole(AriaRole.CHECKBOX).check(new CheckOptions().setForce(rememberUser));
+
+        containerInlineFormCard.getByRole(AriaRole.BUTTON, new Locator.GetByRoleOptions().setName("Submit")).click();
     }
 }
--- playwright-test/src/test/java/com/capgemini/tests/FormLayoutTest.java
+++ playwright-test/src/test/java/com/capgemini/tests/FormLayoutTest.java
@@ -9,15 +9,28 @@ import com.capgemini.pages.NavigationPage;
 public class FormLayoutTest extends BaseTest {
 
     private NavigationPage navPage;
+    
+    FormLayoutPage formPage;
 
     @Test
     void testFillingForm()
     {
-        FormLayoutPage formPage = new FormLayoutPage(getPage());
-
         navPage = new NavigationPage(getPage());
         navPage.navToformLayoutPage();
 
+        formPage = new FormLayoutPage(getPage());
+
         formPage.fillingFormWithEmailAndPassword("test@test.com", "password13", "Option 1");
+    }
+
+    @Test
+    void testFillingInlineForm()
+    {
+        navPage = new NavigationPage(getPage());
+        navPage.navToformLayoutPage();
+
+        formPage = new FormLayoutPage(getPage());
+
+        formPage.fillingInlineFormWithEmailAndPassword("Anakin Skywalker", "test@test.com", true);
     }
 }
